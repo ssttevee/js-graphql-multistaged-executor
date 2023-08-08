92b39ffff1e8bf4a7141c091c6fd273762c8fcc9
Steve Lam <stevyo99@yahoo.ca>
2023-08-08 12:26:27 -0700

    don't need hardcoded declarations anymore

--- dist/backends/faunadb.d.ts
+++
@@ -1,6 +0,0 @@
-import type { ClientConfig, Expr } from "faunadb";
-import type { ExecutorBackend } from "../lib";
-
-export default function createExecutorBackend(
-  opts?: ClientConfig,
-): ExecutorBackend<Expr>;
